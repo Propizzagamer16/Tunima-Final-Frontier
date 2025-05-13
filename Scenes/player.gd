62f01f2f06471b049ca08938810c3db6b8cde7be
Natu <mspheartsne10@gmail.com>
2025-05-12 21:36:06 -0600

    Backgrounds Testing

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
