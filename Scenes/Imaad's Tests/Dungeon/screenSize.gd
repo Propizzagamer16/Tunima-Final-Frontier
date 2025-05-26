7051216ef946181a49953e82d04e2b4737fd4bc6
Emu <imaadmirza27@gmail.com>
2025-05-26 12:46:21 -0600

    Dungeon change
    changed and edited a bunch of the scenes in dungeon layout: going to grab the script of the person for later and resize it so that it will fit into it, will learn how to resize window and make it fit back into the original 128 x 128 file size.

--- Scenes/Imaad's Tests/Dungeon/screenSize.gd
+++ Scenes/Imaad's Tests/Dungeon/screenSize.gd
@@ -1,1 +1,19 @@
-extends Node
+extends Node2D
+
+var original_size: Vector2i
+
+func _ready():
+	# Store the original window size
+	original_size = DisplayServer.window_get_size()
+
+	# Set new temporary window size
+	DisplayServer.window_set_size(Vector2i(800, 450))
+
+	# Optional: Re-center the window
+	DisplayServer.window_set_position(DisplayServer.screen_get_size() / 2 - Vector2i(400, 225))
+
+
+func _exit_tree():
+	# Restore the original window size when the scene tree is exiting
+	DisplayServer.window_set_size(original_size)
+	DisplayServer.window_set_position(DisplayServer.screen_get_size() / 2 - original_size / 2)
