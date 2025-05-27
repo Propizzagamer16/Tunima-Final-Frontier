c6ab9ff640f2d5124abb045bc76f0c10ebe5de6b
Natu <mspheartsne10@gmail.com>
2025-05-26 18:00:30 -0600

    Started Teleport Boss

---
+++ Scenes/Natu's Tests/Boss 2/idle.gd
@@ -0,0 +1,19 @@
+extends State
+
+
+@onready var collision = $"../../player_detection/CollisionShape2D"
+@onready var progress_bar = owner.find_child("ProgressBar")
+
+var player_entered: bool = false:
+	set(value):
+		player_entered = value
+		collision.set_deferred("disabled", value)
+		progress_bar.set_deferred("visible", value)
+	
+
+func _on_player_detection_body_entered(body: Node2D) -> void:
+	player_entered = true
+	
+func transition():
+	if player_entered:
+		get_parent().change_state("Attack")
