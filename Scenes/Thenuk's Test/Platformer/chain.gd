5311467175526848da1126e913afc566d6a678cd
Propizzagamer16 <thenukf12@gmail.com>
2025-06-02 12:36:40 -0600

    jumppad ppoop

--- Scenes/Thenuk's Test/Platformer/chain.gd
+++ Scenes/Thenuk's Test/Platformer/chain.gd
@@ -3,4 +3,3 @@ extends Node2D
 func _process(_delta):
 	if $Area2D.overlaps_body($"../../player") and Input.is_action_pressed("ui_W"):
 		ChainGlobal.ChainOverlaped()
-		
--- Scenes/Thenuk's Test/Platformer/jump_pad.gd
+++ Scenes/Thenuk's Test/Platformer/jump_pad.gd
@@ -1,7 +1,5 @@
 extends Node2D
-@export var force = -500
 
 func _process(_delta):
 	if $Area2D.overlaps_body($"../../player"):
 		JumpPadGlobal.jumpOverlaped()
-		print("hi")
