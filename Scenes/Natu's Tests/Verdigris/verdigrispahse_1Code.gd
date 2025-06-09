d0dc963b043b909de1c60bd0a6ca449eda99bdff
Natu <mspheartsne10@gmail.com>
2025-06-09 16:41:34 -0600

    im tired

---
+++ Scenes/Natu's Tests/Verdigris/verdigrispahse_1Code.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var player = get_node("player")
+@onready var boss = get_node("Verdigris")
+
+func _ready():
+	player.set_camera("Verdigris Phase 1")
+	Global.player_type = "Side"
+	player.set_mode_from_global()
+	player.JUMP_VELOCITY = -1300
