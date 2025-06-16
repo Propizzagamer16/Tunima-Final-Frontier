520838a1f3081f0390f9c3d8918ea8b5913f1477
Propizzagamer16 <thenukf12@gmail.com>
2025-06-16 16:00:49 -0600

    h

---
+++ Scenes/Natu's Tests/Boss 2/verdigrisattack.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var speed := 300
+var direction = Vector2.ZERO
+
+func _physics_process(delta):
+	position += direction * speed * delta
+
+func _on_area_entered(area):
+	if area.name == "Player":
+		area.take_damage(1)
+		queue_free()
