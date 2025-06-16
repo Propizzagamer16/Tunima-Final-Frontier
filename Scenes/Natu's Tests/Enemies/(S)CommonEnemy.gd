147b60cc9191bc5d6bc3b14ed5bd389932dd344a
Natu <mspheartsne10@gmail.com>
2025-06-16 15:09:08 -0600

    hj

--- Scenes/Natu's Tests/Enemies/(S)CommonEnemy.gd
+++ Scenes/Natu's Tests/Enemies/(S)CommonEnemy.gd
@@ -50,6 +50,7 @@ func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
 func take_damage(current_damage):
 	if can_take_damage == true:
 		health = health - current_damage
+		print(health)
 		if health <= 0:
 			self.queue_free()
 		$take_damage_cooldown.start()
